83babaa133dbb17d0a8a88b5370d299ebed9c540
Tung Nguyen <tungnguyen@Tungs-iMac.local>
2022-04-23 00:21:30 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Foo from './Foo';
+import { useState } from 'react';
 
-function App() {
+function App(props) {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="count">Count : {count}</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          props.onChange(count + 1);
+        }}
+        id="buttonClick"
+      >Click me</button>
     </div>
+    
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { unmountComponentAtNode } from 'react-dom';
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('change value when clicked', () => {
+  const onChange = jest.fn();
+  act(() => {
+    render(<App onChange={onChange} />, container);
+  });
+
+  const button = document.getElementById('buttonClick');
+  expect(button.innerHTML).toBe("Click me");
+  
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(onChange).toHaveBeenCalledTimes(1);
+  expect(document.getElementById('count').innerHTML).toBe('Count : 1');
+
+  act(() => {
+    for (let i = 0; i < 5; i++) {
+      button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    }
+  });
+  expect(onChange).toHaveBeenCalledTimes(6);
+});
+
