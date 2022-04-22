6b642e3ecae52816094f14b6129c9f28abf70f8f
Tung Nguyen <tungnguyen@Tungs-iMac.local>
2022-04-23 00:21:46 +0700

    init project

---
+++ src/Foo.js
@@ -0,0 +1,11 @@
+function Foo() {
+    return (
+        <div>
+            <div className="foo-bar">1</div>
+            <div className="foo-bar">1</div>
+            <div className="foo-bar">1</div>
+        </div>
+    )
+}
+
+export default Foo;
